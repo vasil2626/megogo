a840d7478751dde06a1b1c08a2f55e933a0e8542
vasil2626 <vasil1995.17@gmail.com>
2021-11-27 17:18:10 +0400

    addet-bottom-slider

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -155,7 +155,7 @@ const Tabs = () => {
             <div className={`movies__tab-content ${getActiveClass(3, "movies__active-content")}`}>
                 <div className="movie__trailer">
                     <ReactHlsPlayer
-                        src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
+                        src="https://vs431.vcdn.biz/d823c226f41dc6d2d64ba0e8d3a6447a_mgg/vod/hls/b/450_900_1350_1500_2000_5000/u_sid/0/o/129225081/u_uid/0/u_vod/1/u_device/cms_html5/u_devicekey/_site/a/0/type.amlst/playlist.m3u8"
                         autoPlay={false}
                         controls={true}
                         width="100%"
